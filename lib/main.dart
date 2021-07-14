ee805103f6683d1c711963dd1c38b2c99f657d10
iTzZCHIP <51890916+iTzZCHIP@users.noreply.github.com>
2021-07-14 15:42:56 +0200

    first commti

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        
+        
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: LikeButton(
+              circleColor:
+                  CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+              bubblesColor: BubblesColor(
+                dotPrimaryColor: Colors.red,
+                dotSecondaryColor: Color(0xff0099cc),
+              ),
+              likeBuilder: (bool isLiked) {
+                return Icon(
+                  Icons.favorite,
+                  color: isLiked ? Colors.redAccent : Colors.grey,
+                );
+              },
+              likeCount: 665,
+            ),
+          ),
+          SizedBox(width: 20,),
+          Center(
+            child: LikeButton(
+            circleColor:
+                CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+            bubblesColor: BubblesColor(
+              dotPrimaryColor: Color(0xff33b5e5),
+              dotSecondaryColor: Color(0xff0099cc),
+            ),
+            likeBuilder: (bool isLiked) {
+              return Icon(
+                Icons.home,
+                color: isLiked ? Colors.deepPurpleAccent : Colors.grey,
+              );
+            },
+            likeCount: 99,
+            ),
+          ),
+          SizedBox(width: 20,),
+          Center(
+            child: LikeButton(
+            circleColor:
+                CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+            bubblesColor: BubblesColor(
+              dotPrimaryColor: Colors.greenAccent,
+              dotSecondaryColor: Color(0xff0099cc),
+            ),
+            likeBuilder: (bool isLiked) {
+              return Icon(
+                Icons.audiotrack,
+                color: isLiked ? Colors.green : Colors.grey,
+              );
+            },
+            likeCount: 25,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
